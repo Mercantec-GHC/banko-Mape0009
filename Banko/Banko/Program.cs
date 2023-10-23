981dcb053df3c69b81067b1b85dacefa753a3bf5
Mape0009 <mape0009@edu.mercantec.dk>
2023-10-23 10:22:59 +0200

    Progress dag 1 projekt

--- Banko/Banko/Program.cs
+++ Banko/Banko/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> plade1Række1 = new List<string> {"20", "30", "43", "55", "70"};
+List<string> plade1Række2 = new List<string> { "4", "14", "33", "44", "67"};
+List<string> plade1Række3 = new List<string> {"29", "49", "68", "78", "87"};
+
+string trukketTal = "0";
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(plade1Række1[i]+ " ");
+}
+Console.WriteLine();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(plade1Række2[i]+ " ");
+}
+Console.WriteLine();
+for (int i=0; i < 5; i++)
+{
+    Console.Write(plade1Række3[i] + " ");
+}
+Console.WriteLine();
+
+trukketTal = Console.ReadLine();
+
+if (plade1Række1.Contains(trukketTal))
+{
+    int skiftTal = plade1Række1.IndexOf(trukketTal);
+    plade1Række1[skiftTal]="X";
+    for (int i = 0; i < 5;i++)
+    {
+        Console.Write(plade1Række1[i] + " ");
+    }
+}
+
