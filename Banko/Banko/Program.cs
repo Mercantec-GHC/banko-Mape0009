7e1757d192522e2514bfa36668d7abe8336103d6
Mape0009 <mape0009@edu.mercantec.dk>
2023-10-23 11:11:13 +0200

    Projekt dag 1

--- Banko/Banko/Program.cs
+++ Banko/Banko/Program.cs
@@ -3,32 +3,43 @@ List<string> plade1Række2 = new List<string> { "4", "14", "33", "44", "67"};
 List<string> plade1Række3 = new List<string> {"29", "49", "68", "78", "87"};
 
 string trukketTal = "0";
+bool fuldPlade = false;
 
-for (int i = 0; i < 5; i++)
+do
 {
-    Console.Write(plade1Række1[i]+ " ");
-}
-Console.WriteLine();
-for (int i = 0; i < 5; i++)
-{
-    Console.Write(plade1Række2[i]+ " ");
-}
-Console.WriteLine();
-for (int i=0; i < 5; i++)
-{
-    Console.Write(plade1Række3[i] + " ");
-}
-Console.WriteLine();
 
-trukketTal = Console.ReadLine();
-
-if (plade1Række1.Contains(trukketTal))
-{
-    int skiftTal = plade1Række1.IndexOf(trukketTal);
-    plade1Række1[skiftTal]="X";
-    for (int i = 0; i < 5;i++)
+    for (int i = 0; i < 5; i++)
     {
         Console.Write(plade1Række1[i] + " ");
     }
-}
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade1Række2[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade1Række3[i] + " ");
+    }
+    Console.WriteLine();
 
+    trukketTal = Console.ReadLine();
+
+    if (plade1Række1.Contains(trukketTal))
+    {
+        int skiftTal = plade1Række1.IndexOf(trukketTal);
+        plade1Række1[skiftTal] = "X";
+    }
+    else if (plade1Række2.Contains(trukketTal))
+    {
+        int skiftTal = plade1Række2.IndexOf(trukketTal);
+        plade1Række2[skiftTal] = "X";
+    }
+    else if (plade1Række3.Contains(trukketTal))
+    {
+        int skiftTal = plade1Række3.IndexOf(trukketTal);
+        plade1Række3[skiftTal] = "X";
+    }
+    Console.Clear();
+} while (fuldPlade == false);
