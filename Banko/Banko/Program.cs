19f2de86a69cee579eeb2c33c7158e64877b3278
Mape0009 <mape0009@edu.mercantec.dk>
2023-10-24 14:57:30 +0200

    Dag 2 projekt Banko

--- Banko/Banko/Program.cs
+++ Banko/Banko/Program.cs
@@ -2,7 +2,10 @@
 List<string> plade1Række1 = new List<string> {"20", "30", "43", "55", "70"};
 List<string> plade1Række2 = new List<string> { "4", "14", "33", "44", "67"};
 List<string> plade1Række3 = new List<string> {"29", "49", "68", "78", "87"};
-
+//ID: 2
+List<string> plade2Række1 = new List<string> {"24", "30", "40", "54", "64" };
+List<string> plade2Række2 = new List<string> {"18", "27", "42", "65", "71" };
+List<string> plade2Række3 = new List<string> {"7", "43", "57", "69", "89" };
 
 string trukketTal = "0";
 int skiftTal;
@@ -17,7 +20,7 @@ do
     {
         Console.Write(plade1Række1[i] + " ");
     }
-    Console.WriteLine();
+   Console.WriteLine();
     for (int i = 0; i < 5; i++)
     {
         Console.Write(plade1Række2[i] + " ");
@@ -28,33 +31,72 @@ do
         Console.Write(plade1Række3[i] + " ");
     }
     Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række1[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række2[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række3[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
 
     //En variabel, som bliver brugt til at ændre i listerne
+    Console.Write("Indsæt tallene her: ");
     trukketTal = Console.ReadLine();
 
 
-    if (plade1Række1.Contains(trukketTal))
+    if (plade1Række1.Contains(trukketTal) || plade2Række1.Contains(trukketTal))
     {
         skiftTal = plade1Række1.IndexOf(trukketTal);
-        plade1Række1[skiftTal] = "X";
+        if (skiftTal != -1) 
+            plade1Række1[skiftTal] = "X";
+        skiftTal = plade2Række1.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række1[skiftTal] = "X";
     }
-    else if (plade1Række2.Contains(trukketTal))
+    else if (plade1Række2.Contains(trukketTal) || plade2Række2.Contains(trukketTal))
     {
         skiftTal = plade1Række2.IndexOf(trukketTal);
-        plade1Række2[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række2[skiftTal] = "X";
+        skiftTal = plade2Række2.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række2[skiftTal] = "X";
     }
-    else if (plade1Række3.Contains(trukketTal))
+    else if (plade1Række3.Contains(trukketTal ) || plade2Række3.Contains(trukketTal))
     {
         skiftTal = plade1Række3.IndexOf(trukketTal);
-        plade1Række3[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række3[skiftTal] = "X";
+        skiftTal = plade2Række3.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række3[skiftTal] = "X";
     }
 
     if (plade1Række1.All(x => x == "X") || plade1Række2.All(x => x == "X") || plade1Række3.All(x => x == "X"))
     {
-        Console.WriteLine("Du har en fuld række");
+        Console.WriteLine("Du har en fuld række på Plade: 1");
         fuldrække1 = true;
+        Console.WriteLine("Tryk enter for at fortsætte");
         Console.ReadLine();
-        Console.WriteLine("Tryk for at fortsætte");
+        Console.Clear();
+    }
+
+    else if (plade2Række1.All(x => x == "X") || plade2Række2.All(x => x == "X") || plade2Række3.All(x => x == "X"))
+    {
+        Console.WriteLine("Du har en fuld række på Plade: 2");
+        fuldrække1 = true;
+        Console.WriteLine("Tryk enter for at fortsætte");
+        Console.ReadLine();        
         Console.Clear();
     }
 
@@ -84,36 +126,75 @@ do
         Console.Write(plade1Række3[i] + " ");
     }
     Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række1[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række2[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række3[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
 
+    Console.Write("Indsæt tallene her: ");
     trukketTal = Console.ReadLine();
 
-    if (plade1Række1.Contains(trukketTal))
+    if (plade1Række1.Contains(trukketTal) || plade2Række1.Contains(trukketTal))
     {
         skiftTal = plade1Række1.IndexOf(trukketTal);
-        plade1Række1[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række1[skiftTal] = "X";
+        skiftTal = plade2Række1.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række1[skiftTal] = "X";
     }
-    else if (plade1Række2.Contains(trukketTal))
+    else if (plade1Række2.Contains(trukketTal) || plade2Række2.Contains(trukketTal))
     {
         skiftTal = plade1Række2.IndexOf(trukketTal);
-        plade1Række2[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række2[skiftTal] = "X";
+        skiftTal = plade2Række2.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række2[skiftTal] = "X";
     }
-    else if (plade1Række3.Contains(trukketTal))
+    else if (plade1Række3.Contains(trukketTal) || plade2Række3.Contains(trukketTal))
     {
         skiftTal = plade1Række3.IndexOf(trukketTal);
-        plade1Række3[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række3[skiftTal] = "X";
+        skiftTal = plade2Række3.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række3[skiftTal] = "X";
     }
 
-    if ((plade1Række1.All(x => x == "X") && plade1Række2.All(x => x == "X")) ||
+    if ((plade1Række1.All(x => x == "X") && plade1Række2.All(x => x == "X") ||
     (plade1Række2.All(x => x == "X") && plade1Række3.All(x => x == "X")) ||
-    (plade1Række3.All(x => x == "X") && plade1Række1.All(x => x == "X")))
+    (plade1Række3.All(x => x == "X") && plade1Række1.All(x => x == "X"))))
     {
-        Console.WriteLine("Du har to fulde rækker");
+        Console.WriteLine("Du har to fulde rækker på Plade: 1");
         fuldrække2 = true;
+        Console.WriteLine("Tryk enter for at fortsætte");
         Console.ReadLine();
-        Console.WriteLine("Tryk for at fortsætte");
         Console.Clear();
     }
-
+    if ((plade2Række1.All(x => x == "X") && plade2Række2.All(x => x == "X") ||
+    (plade2Række2.All(x => x == "X") && plade2Række3.All(x => x == "X")) ||
+    (plade2Række3.All(x => x == "X") && plade2Række1.All(x => x == "X"))))
+    {
+        Console.WriteLine("Du har to fulde rækker på Plade: 2");
+        fuldrække2 = true;
+        Console.WriteLine("Tryk enter for at fortsætte");
+        Console.ReadLine();
+        Console.Clear();
+    }
 
     else
     {
@@ -140,31 +221,69 @@ do
         Console.Write(plade1Række3[i] + " ");
     }
     Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række1[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række2[i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write(plade2Række3[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
 
+    Console.Write("Indsæt tallene her: ");
     trukketTal = Console.ReadLine();
 
-    if (plade1Række1.Contains(trukketTal))
+    if (plade1Række1.Contains(trukketTal) || plade2Række1.Contains(trukketTal))
     {
         skiftTal = plade1Række1.IndexOf(trukketTal);
-        plade1Række1[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række1[skiftTal] = "X";
+        skiftTal = plade2Række1.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række1[skiftTal] = "X";
     }
-    else if (plade1Række2.Contains(trukketTal))
+    else if (plade1Række2.Contains(trukketTal) || plade2Række2.Contains(trukketTal))
     {
         skiftTal = plade1Række2.IndexOf(trukketTal);
-        plade1Række2[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række2[skiftTal] = "X";
+        skiftTal = plade2Række2.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række2[skiftTal] = "X";
     }
-    else if (plade1Række3.Contains(trukketTal))
+    else if (plade1Række3.Contains(trukketTal) || plade2Række3.Contains(trukketTal))
     {
         skiftTal = plade1Række3.IndexOf(trukketTal);
-        plade1Række3[skiftTal] = "X";
+        if (skiftTal != -1)
+            plade1Række3[skiftTal] = "X";
+        skiftTal = plade2Række3.IndexOf(trukketTal);
+        if (skiftTal != -1)
+            plade2Række3[skiftTal] = "X";
     }
 
     if (plade1Række1.All(x => x == "X") && plade1Række2.All(x => x == "X") && plade1Række3.All(x=> x == "X"))
     {
-        Console.WriteLine("Du har fuld plade");
+        Console.WriteLine("Du har fuld plade på Plade: 1");
         fuldPlade = true;
+        Console.WriteLine("Tryk enter for at fortsætte");
         Console.ReadLine();
-        Console.WriteLine("Tryk for at fortsætte");
+        Console.Clear();
+    }
+    else if (plade2Række1.All(x => x == "X") && plade2Række2.All(x => x == "X") && plade2Række3.All(x => x == "X"))
+    {
+        Console.WriteLine("Du har fuld plade på Plade: 2");
+        fuldPlade = true;
+        Console.WriteLine("Tryk enter for at fortsætte");
+        Console.ReadLine();
         Console.Clear();
     }
 
@@ -173,3 +292,4 @@ do
         Console.Clear();
     }
 } while (fuldPlade == false);
+   
